269056f6df020ee370af2b6c3e17a8bdc2b97dd0
Ya Wang <wangy1@mskcc.org>
2011-02-23 15:40:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amy02::Application.config.secret_token = '56fdc7a873fa27c5ffa3e4eded893ac5a32ad7d4be501184ae3fb6d025db09fa18e801e0ae992361c90fe7d46d90c44befe9a2637162db03ee7c1268bf5fe48d'
